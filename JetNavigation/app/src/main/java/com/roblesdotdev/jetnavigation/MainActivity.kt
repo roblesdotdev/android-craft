edb130bcaf0686d5b0aa99ae3c0951980cbf80a6
roblesdotdev <httpsnip@gmail.com>
2023-12-15 15:20:42 -0300

    JetNavigation: initial setup

---
+++ JetNavigation/app/src/main/java/com/roblesdotdev/jetnavigation/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.roblesdotdev.jetnavigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.roblesdotdev.jetnavigation.ui.theme.JetNavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetNavigationTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "list_screen"
+                ) {
+                    composable("list_screen") {
+                        Text(text = "Hello, World")
+                    }
+                }
+
+            }
+        }
+    }
+}
