13e0e3d74a53c093e509f8066cd20be46af3a9a0
roblesdotdev <httpsnip@gmail.com>
2023-12-15 16:59:26 -0300

    JetNavigation: arguments

--- JetNavigation/app/src/main/java/com/roblesdotdev/jetnavigation/MainActivity.kt
+++ JetNavigation/app/src/main/java/com/roblesdotdev/jetnavigation/MainActivity.kt
@@ -3,10 +3,16 @@ package com.roblesdotdev.jetnavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.remember
+import androidx.navigation.NavArgument
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.roblesdotdev.jetnavigation.ui.theme.JetNavigationTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,7 +28,26 @@ class MainActivity : ComponentActivity() {
                     startDestination = "list_screen"
                 ) {
                     composable("list_screen") {
-                        Text(text = "Hello, World")
+                        val message = "This is my message"
+                        Column {
+                            Text(text = "Hello, World")
+                            Button(onClick = { navController.navigate("list_detail/$message" )}) {
+                               Text(text = "Go to detail")
+                            }
+                        }
+                    }
+                    composable(
+                        route = "list_detail/{message}",
+                        arguments = listOf(
+                            navArgument("message") {
+                                type = NavType.StringType
+                            }
+                        )
+                    ) {
+                        val message = remember {
+                            it.arguments?.getString("message")
+                        }
+                        Text(text = "Message: $message")
                     }
                 }
 
