7016581cd89038a28de9f90ad4c62681b11144da
roblesdotdev <httpsnip@gmail.com>
2023-12-15 16:59:48 -0300

    JetNavigation: refactor

--- JetNavigation/app/src/main/java/com/roblesdotdev/jetnavigation/MainActivity.kt
+++ JetNavigation/app/src/main/java/com/roblesdotdev/jetnavigation/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.remember
-import androidx.navigation.NavArgument
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
